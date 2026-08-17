a7b545c773965253aede65d40dc5353ad7639af7
maoxiansheng946-dev <maoxiansheng946@gmail.com>
2026-08-17 20:01:40 +0800

    chore: point website to migrated LumiOS repository

--- site-config.js
+++ site-config.js
@@ -1,8 +1,8 @@
 window.LUMI_SITE = Object.freeze({
-  repository: "https://github.com/maoxiansheng946-dev/-lumi-OS",
-  repositoryApi: "https://api.github.com/repos/maoxiansheng946-dev/-lumi-OS",
-  releases: "https://github.com/maoxiansheng946-dev/-lumi-OS/releases",
+  repository: "https://github.com/5pyj29s7cm-tech/LumiOS",
+  repositoryApi: "https://api.github.com/repos/5pyj29s7cm-tech/LumiOS",
+  releases: "https://github.com/5pyj29s7cm-tech/LumiOS/releases",
   download: "https://releases.lumiai.asia/lumi-os/latest/download",
-  contact: "https://github.com/maoxiansheng946-dev/-lumi-OS/issues",
+  contact: "https://github.com/5pyj29s7cm-tech/LumiOS/issues",
   fallbackStars: 263
 });
